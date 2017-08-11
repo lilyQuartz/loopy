f913619a1570896f26d51ceb1bb9757a8554e7f8
Lily Quartz <lilyquartz@gmail.com>
2017-08-11 01:18:53 -0700

    Update Sidebar.js
    Tried uncommenting the third line

--- v1.1/js/Sidebar.js
+++ v1.1/js/Sidebar.js
@@ -183,7 +183,7 @@ function Sidebar(loopy){
 				
 			"<a target='_blank' href='../'>Simple Directed Graph</a> is "+
 			"made by <a target='_blank' href='http://lilyquartz.github.io'>lilyQuartz github</a> "+
-			//"with your support <a target='_blank' href='https://www.patreon.com/ncase'>on patreon</a> &lt;3<br><br>"+
+			"with your support <a target='_blank' href='https://www.patreon.com/ncase'>on patreon</a> &lt;3<br><br>"+
 			//"<span style='font-size:0.85em'>P.S: go read <a target='_blank' href='https://www.amazon.com/Thinking-Systems-Donella-H-Meadows/dp/1603580557'>Thinking In Systems</a>, thx</span>"
 
 		}));
