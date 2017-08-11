669e587b6eb23b5381c85b036aa181f2e9161524
Lily Quartz <lilyquartz@gmail.com>
2017-08-11 03:57:32 -0700

    Update Node.js
    changed r in the radius while loop to self.radius so the radius doesn't grow so much

--- v1.1/js/Node.js
+++ v1.1/js/Node.js
@@ -226,7 +226,7 @@ function Node(model, config){
 		_r = _r/(Math.PI/2);
 		_r = (_r+1)/2;
 
-		while(width > r*2 - 30){ // -30 for buffer. HACK: HARD-CODED.
+		while(width > self.radius*2 - 30){ // -30 for buffer. HACK: HARD-CODED.
 			self.radius = self.radius + ctx.measureText(self.label).width;
 		} 
 		 //Maybe this will work maybe not
