f10a1c0efc2d13ce9e841d596374329199920f78
Lily Quartz <lilyquartz@gmail.com>
2017-08-11 18:46:43 -0700

    Update Node.js
    Try adding the zoom button to node

--- v1.1/js/Node.js
+++ v1.1/js/Node.js
@@ -172,6 +172,41 @@ function Node(model, config){
 
 	};
 
+	//Zoom Button, Experimental
+	
+	// PLAY BUTTON's keyboard shortcut
+	// TODO: Toggle back & forth??????
+	subscribe("key/enter",function(){
+		if(Key.control){ // Ctrl-Enter or ⌘-Enter
+			//loopy.setMode(Loopy.MODE_PLAY);
+		}
+	}); 
+	// During the Editor
+	(function(){
+		var page = new Page();
+		// PLAY BUTTON
+		var buttonDOM = page.addComponent(new PlayButton({
+			icon: 0,
+			label: "Zoom",
+			tooltip: isMacLike ? "⌘-Enter" : "control-enter",
+			onclick: function(){
+				//loopy.setMode(Loopy.MODE_PLAY);
+				//self.showPage("Edit");
+			}
+		})).dom;
+		buttonDOM.setAttribute("big","yes");
+		buttonDOM.style.fontSize = "28px";
+		buttonDOM.style.height = "35px";
+		self.addPage("Editor", page);
+	})();
+	function PlayButton(config){
+	var self = this;
+	var label = "<div class='play_button_icon' icon='"+config.icon+"'></div> "
+				+ "<div class='play_button_label'>"+config.label+"</div>";
+	self.dom = _createButton(label, function(){
+		config.onclick();
+	});
+	
 	// Draw
 	var _circleRadius = 0;
 	self.draw = function(ctx){
